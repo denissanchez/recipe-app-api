ab47d5832fbc4c61318986e3b98125b96a0f1825
Denis Sanchez <desanchezca@hotmail.com>
2022-01-04 07:55:57 -0500

    Added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -5,10 +5,21 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
         """Creates and saves a new user"""
-        user = self.model(email=email, **kwargs)
+        if not email:
+            raise ValueError("Users must have an email address.")
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,24 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)        
         self.assertTrue(user.check_password(password))     
 
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@EXAMPLE.COM'
+        user = get_user_model().objects.create_user(email, 'top_secr3tt')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'top_secr3tt')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'top_secr3tt'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
