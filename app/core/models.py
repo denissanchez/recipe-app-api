03095bb7114a5ca528db67e0f731f3cf07866903
Denis Sanchez <desanchezca@hotmail.com>
2022-01-08 10:03:50 -0500

    Added endpoint to list and create ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -33,6 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+
 class Tag(models.Model):
     """Tag to be used for a recipe"""
     user = models.ForeignKey(
@@ -40,6 +41,18 @@ class Tag(models.Model):
         on_delete=models.CASCADE
     )
     name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient to be used in a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self) -> str:
         return self.name
--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.db.models import query_utils
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipes import serializers
 
 class TagViewSet(viewsets.GenericViewSet,
@@ -13,6 +14,21 @@ class TagViewSet(viewsets.GenericViewSet,
     permission_classes = [IsAuthenticated,]
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated, ]
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
