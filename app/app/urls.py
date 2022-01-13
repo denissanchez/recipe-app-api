538cb3b3caeb61a42e684e32aa16da8172f44a27
Denis Sanchez <desanchezca@hotmail.com>
2022-01-12 21:51:17 -0500

    Added image field to recipe model

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
         path('users/', include('users.urls', namespace='users')),
         path('recipes/', include('recipes.urls', namespace='recipes')),
-    ]))
-]
+    ])),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,6 @@
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
@@ -58,6 +61,12 @@ class Ingredient(models.Model):
         return self.name
 
 
+def recipe_image_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/', filename)
+
+
 class Recipe(models.Model):
     """Recipe object"""
     user = models.ForeignKey(
@@ -70,6 +79,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self) -> str:
         return self.title
