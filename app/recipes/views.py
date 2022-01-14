037e0183c937274d7edd20f0c5233ab0feeed3f6
Denis Sanchez <desanchezca@hotmail.com>
2022-01-13 20:52:43 -0500

    Course completed! 翿翿

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -15,7 +15,18 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = [IsAuthenticated,]
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+
+        queryset = self.queryset
+
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -45,8 +56,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeImageSerializer
         return self.serializer_class
 
+    def _params_to_ints(self, str_ids):
+        return [int(str_id) for str_id in str_ids.split(',')]
+
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+
+        queryset = self.queryset
+
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
